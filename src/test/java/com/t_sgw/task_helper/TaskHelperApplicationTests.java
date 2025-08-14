152a87ad98880644dcdd002589bb8a74845a7d88
T-SGW <sgwtthkiviiv@gmail.com>
2025-08-14 18:25:56 +0900

    Initial commit

---
+++ src/test/java/com/t_sgw/task_helper/TaskHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t_sgw.task_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
